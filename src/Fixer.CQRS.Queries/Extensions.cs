123f3c1876c9a51e8bcaab093260106d24ed5585
Samin <Samin@DESKTOP-B8FQBKL>
2020-08-17 22:37:14 +0430

    first commit

---
+++ src/Fixer.CQRS.Queries/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Fixer.CQRS.Queries.Dispatchers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fixer.CQRS.Queries
+{
+    public static class Extensions
+    {
+        public static IFixerBuilder AddQueryHandlers(this IFixerBuilder builder)
+        {
+            builder.Services.Scan(s =>
+                s.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                    .AddClasses(c => c.AssignableTo(typeof(IQueryHandler<,>)))
+                    .AsImplementedInterfaces()
+                    .WithTransientLifetime());
+
+            return builder;
+        }
+
+        public static IFixerBuilder AddInMemoryQueryDispatcher(this IFixerBuilder builder)
+        {
+            builder.Services.AddSingleton<IQueryDispatcher, QueryDispatcher>();
+            return builder;
+        }
+    }
+}
---
+++ src/Fixer.CQRS.Queries/IQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fixer.CQRS.Queries
+{
+    //Marker
+    public interface IQuery
+    {
+    }
+
+    public interface IQuery<T> : IQuery
+    {
+    }
+}
